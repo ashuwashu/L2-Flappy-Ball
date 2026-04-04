b4edce279792ef920bf0b2eb5b44e967d3157ffb
ashuwashu <swetha.arun9@gmail.com>
2026-04-04 11:53:29 +0200

    Add files via upload

---
+++ Ball.py
@@ -0,0 +1,13 @@
+import pgzrun
+from random import randint 
+
+WIDTH=800 
+HEIGHT=600
+TITLE="Flappy Ball Game"
+
+R=randint(0, 255)
+G=randint(0, 255)
+B=randint(0,255)
+
+clr=R, G, B
+Gravity=2000
