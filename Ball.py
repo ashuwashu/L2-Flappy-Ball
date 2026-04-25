c5125b6ae238ae5f47ccacae40754851da5581fb
ashuwashu <swetha.arun9@gmail.com>
2026-04-25 11:13:26 +0200

    Updated Ball.py

--- Ball.py
+++ Ball.py
@@ -11,3 +11,50 @@ B=randint(0,255)
 
 clr=R, G, B
 Gravity=2000
+
+class Ball:
+    def __init__(self, initial_x, initial_y):
+        self.x=initial_x
+        self.y=initial_y
+        self.vx=200
+        self.vy=0
+        self.radius=40
+    
+    def draw(self):
+        pos=(self.x, self.y)
+        screen.draw.filled_circle(pos, self.radius, clr)
+        
+
+ball=Ball(400,10)
+def draw():
+    screen.clear()
+    ball.draw()
+
+def on_key_down(key):
+    if key==keys.SPACE:
+        ball.vy=-500
+
+def update(dt):
+    uy=ball.vy
+    ball.vy +=Gravity*dt
+    ball.y +=(uy + ball.vy)*0.5*dt
+    
+    if ball.y > HEIGHT-ball.radius:
+        ball.y=HEIGHT-ball.radius
+        ball.vy=ball.vy*0.9
+
+        
+    ball.x += ball.vx * dt  
+    if ball.x > WIDTH - ball.radius or ball.x < ball.radius:
+        ball.vx= -ball.vx
+    
+
+
+    
+
+
+
+
+    
+
+pgzrun.go()
